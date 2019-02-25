348f88c9034bdf886415aecdbe7fde19486c1efb
vale75020 <valentinosuma@hotmail.com>
2019-02-25 17:28:05 +0100

    admincards

--- client/src/components/ADMINCARDS/AdminCards.js
+++ client/src/components/ADMINCARDS/AdminCards.js
@@ -5,12 +5,12 @@ class AdminCards extends Component {
     render() {
         return (
             <div>
-                <div style={{width: '80%', margin: 'auto'}}>
+                <div style={{width: '90%', margin: 'auto', fontSize:"20px"}}>
     <Grid className="demo-grid-ruler">
-        <Cell col={3}>Valider</Cell>
-        <Cell col={3}>To Do</Cell>
-        <Cell col={3}>Doing</Cell>
-        <Cell col={3}>Done</Cell>
+        <Cell  style={{backgroundColor:"grey", height:"100vh", padding:"10px"}} col={3}>Valider</Cell>
+        <Cell  style={{backgroundColor:"grey", height:"100vh", padding:"10px"}} col={3}>To Do</Cell>
+        <Cell  style={{backgroundColor:"grey", height:"100vh", padding:"10px"}} col={3}>Doing</Cell>
+        <Cell  style={{backgroundColor:"grey", height:"100vh", padding:"10px"}} col={3}>Done</Cell>
     </Grid>
             </div>
             </div>
