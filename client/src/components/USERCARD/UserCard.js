abc4e4a1d1d2d5be09beb87b3b93a13bbf3bb369
vale75020 <valentinosuma@hotmail.com>
2019-05-03 11:32:27 +0200

    button card view

--- client/src/components/USERCARD/UserCard.js
+++ client/src/components/USERCARD/UserCard.js
@@ -1,38 +1,74 @@
-import React from 'react'
-import { Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
+import React, { Component } from "react";
+import { Card, CardTitle, CardText, CardActions, Button } from "react-mdl";
 import "./UserCard.css";
 
-const UserCard = (props) => {
-    const {title , text, status} = props
+class UserCard extends Component {
+  state = {
+    isOpen: false
+  };
 
-    function showDetails() {
-    console.log("details")
-    } 
-    
+  showDetails = () => {
+      this.setState({
+         isOpen : !this.state.isOpen
+      })
+    }
+
+  render() {
+    const { title, text, status } = this.props;
+    const mystyle = this.state.isOpen ? { width: "100%", height: "auto", marginTop: "20px" }:{ width: "100%", height: "300px", marginTop: "20px" };
+
     return (
-        <Card shadow={5} style={{width: '100%', height: 'auto', marginTop: '20px'}}>
-            <CardTitle style={{backgroundColor: "#2c3e50", color:"white"}}>
-                <h2 style={{ width:"100%", margin:"0",lineHeight:"inherit", fontSize:"16px"}}>
-                    {title}
-                </h2>
-            </CardTitle>
-            <CardTitle style={{backgroundColor: "#e67e22", color:"black"}}>
-            <h2 style={{margin:"0 auto",lineHeight:"inherit", fontSize:"14px"}}>
-                    {status}
-                </h2>
-            </CardTitle>
-            <CardText style={{padding:"inherit",width:"100%",height: '100%', color: "black", fontSize:"12px"}}>
-                {text}
-            </CardText>
-            <CardActions border style={{backgroundColor: "#2c3e50", color:"white",bottom:0, position:"absolute"}}>
-                <Button colored style={{color:"white"}} onClick={showDetails}>
-                    View
-                </Button>
-            </CardActions>
-        </Card> 
+      <Card shadow={5} style={mystyle}>
+        <CardTitle style={{ backgroundColor: "#2c3e50", color: "white" }}>
+          <h2
+            style={{
+              width: "100%",
+              margin: "0",
+              lineHeight: "inherit",
+              fontSize: "16px"
+            }}
+          >
+            {title}
+          </h2>
+        </CardTitle>
+        <CardTitle style={{ backgroundColor: "#e67e22", color: "black" }}>
+          <h2
+            style={{
+              margin: "0 auto",
+              lineHeight: "inherit",
+              fontSize: "14px"
+            }}
+          >
+            {status}
+          </h2>
+        </CardTitle>
+        <CardText
+          style={{
+            padding: "inherit",
+            width: "100%",
+            height: "100%",
+            color: "black",
+            fontSize: "12px"
+          }}
+        >
+          {text}
+        </CardText>
+        <CardActions
+          border
+          style={{
+            backgroundColor: "#2c3e50",
+            color: "white",
+            bottom: 0,
+            position: "relative"
+          }}
+        >
+          <Button colored style={{ color: "white" }} onClick={this.showDetails}>
+            View
+          </Button>
+        </CardActions>
+      </Card>
     );
-};
-
-
+  }
+}
 
 export default UserCard;
