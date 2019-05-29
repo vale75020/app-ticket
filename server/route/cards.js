97b8eaf7aa18435d769d678e2cfd1087ed34b46f
martineon <martin.eon.lairet@gmail.com>
2019-05-29 15:52:05 +0200

    fix

--- server/route/cards.js
+++ server/route/cards.js
@@ -1,6 +1,6 @@
 const express = require("express"); // import express
 let _cards = require("../cards.json");
-const cors = require('cors')
+const cors = require("cors");
 const Card = require("../models/card.model");
 const passport = require("passport");
 
@@ -11,65 +11,68 @@ app.use(express.json()); // pour parser les données recus par le body en format
 app.use(express.urlencoded({ extended: false }));
 
 function cardExist(id) {
-    const card = _cards.find(card => card.id == id);
-    return card;
-  }
-  
-app.get("/cards", passport.authenticate("jwt", {session: false}), (req, res) => {
+  const card = _cards.find(card => card.id == id);
+  return card;
+}
+
+app.get(
+  "/cards",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
     Card.find({}, (err, card) => {
-      if(err) console.log(err);
-      res.json(card)
-    })
-  });
-  
-  // app.get("/cards/:id", (req, res) => {
-  //   const id = req.params.id; // pour recuperer l'id
-  //   const card = _cards.find(card => card.id == id); // comparer l'id donné avec celui de la card
-  //   if (card) res.status(200).send(card);
-  //   else res.status(404).send("Card not found");
-  //   //console.log(card)
-  // });
-  
-  app.post("/newcard", (req, res) => {
+      if (err) console.log(err);
+      res.json(card);
+    });
+  }
+);
 
-    Card.findOne({
-      title: req.body.title
-    }).then(card => {
-      const newCard = new Card({
-        title: req.body.title,
-        text: req.body.text
-      });
-      card
-        ? res.status(400).json({ title: "la card existe deja !" })
-        : newCard.save()
-                .then(card => res.json(card))
-                .catch(err => console.log(err));
-            });
-          });
-  
-  app.put("/cards/:id", (req, res) => {
-    const id = req.params.id; // verifier que l'id existe
-    const card = cardExist(id); //return la card
-  
-    const updatedFields = ["title", "description"]; // tous les champs que on peut mettre à jour
-  
-    if (card) {
-      const body = req.body; //update
-  
-      updatedFields.forEach(field => {
-        // boucler le tableau champs
-        if (req.body[field]) card[field] = req.body[field];
-      });
-      res.status(200).send(card);
-    } else res.status(404).send("Card not found");
-  });
-  
-  app.delete("/cards/:id", (req, res) => {
-    Card.findOneAndDelete({_id: req.params.id}, (err, card) => {
-      if(err) return console.log("Cette card n'exsite pas");
-      res.json(card)
-    })
-  });
+// app.get("/cards/:id", (req, res) => {
+//   const id = req.params.id; // pour recuperer l'id
+//   const card = _cards.find(card => card.id == id); // comparer l'id donné avec celui de la card
+//   if (card) res.status(200).send(card);
+//   else res.status(404).send("Card not found");
+//   //console.log(card)
+// });
 
+app.post("/newcard", (req, res) => {
+  Card.findOne({
+    title: req.body.title
+  }).then(card => {
+    const newCard = new Card({
+      title: req.body.title,
+      text: req.body.text
+    });
+    card
+      ? res.status(400).json({ title: "la card existe deja !" })
+      : newCard
+          .save()
+          .then(card => res.json(card))
+          .catch(err => console.log(err));
+  });
+});
 
-  module.exports = app
+app.put("/cards/:id", (req, res) => {
+  const id = req.params.id; // verifier que l'id existe
+  const card = cardExist(id); //return la card
+
+  const updatedFields = ["title", "description"]; // tous les champs que on peut mettre à jour
+
+  if (card) {
+    const body = req.body; //update
+
+    updatedFields.forEach(field => {
+      // boucler le tableau champs
+      if (req.body[field]) card[field] = req.body[field];
+    });
+    res.status(200).send(card);
+  } else res.status(404).send("Card not found");
+});
+
+app.delete("/cards/:id", (req, res) => {
+  Card.findOneAndDelete({ _id: req.params.id }, (err, card) => {
+    if (err) return console.log("Cette card n'exsite pas");
+    res.json(card);
+  });
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -5,29 +5,30 @@ let users = require("./route/users.js");
 let cards = require("./route/cards.js");
 const passport = require("passport");
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 var app = express(); // creation app express
-const cors = require('cors') // pour eviter que tous puissent taper sur mon API
-require("./config/passport")(passport)
+const cors = require("cors"); // pour eviter que tous puissent taper sur mon API
+require("./config/passport")(passport);
 
 app.use(passport.initialize());
 app.use(cors()); // pour autoriser le fetch
 app.use(express.json()); // pour parser les données recus par le body en format json
 app.use(express.urlencoded({ extended: false }));
 
-// app.use(express.static(__dirname + '/public/')); 
+// app.use(express.static(__dirname + '/public/'));
 
-app.use("/users", users);
-app.use("/cards/", cards);
+app.use("/", users);
+app.use("/", cards);
 
-mongoose.connect('mongodb://localhost:27017/appticket', { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    console.log('Connecté a MongoDB !')
+mongoose.connect("mongodb://localhost:27017/appticket", {
+  useNewUrlParser: true
 });
-
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("Connecté a MongoDB !");
+});
 
 app.listen(3333, () => {
   console.log("listening on port: " + 3333); //utiliser l'app node app.js pour la lancer
